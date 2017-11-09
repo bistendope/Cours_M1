7ef93bec6f8d61422939bcea72b1289fb19fd346
bistendope <louis.label@wanadoo.fr>
2017-11-09 12:06:34 +0100

    TP2 systeme Ex terminé

--- Systeme_Ex/TD2/exo6/runas.c
+++ Systeme_Ex/TD2/exo6/runas.c
@@ -6,17 +6,20 @@
 #define OFFSET 3
 
 int runas(char *name, char *program, char *args[], int argc){
-	char *myargs[argc];
-	strcpy(myargs[0], name);
-	for(int i=0; i< argc; i++){
-		printf("%s \n", myargs[i]);
-	}
-	for(int i=1; i< argc; i++){
-		strcpy(myargs[i], args[i-1]);
+	char* myargs[argc+1];
+	myargs[0]=name;
+	int i;
+	
+	for(i=0; i< argc+1; i++){
+		myargs[i+1] = args[i];
 	}
 	return execvp(program, myargs);
 }
 
 int main(int argc, char *argv[]){
-	return runas(argv[1], argv[2], argv+OFFSET, argc - OFFSET);
+	if (argc<3) {
+		fprintf(stderr,"usage: %s name program [args...]\n",argv[0]);
+		return 1;
+	}
+	return runas(argv[1], argv[2], argv + OFFSET, argc - OFFSET);
 }
